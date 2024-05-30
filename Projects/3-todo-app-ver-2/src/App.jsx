751ae77bb93873ea812d63c9d8583d34821bc442
Mani1008 <mk114003@gmail.com>
2024-05-30 23:55:15 +0530

    4th update

--- Projects/3-todo-app-ver-2/src/App.jsx
+++ Projects/3-todo-app-ver-2/src/App.jsx
@@ -7,23 +7,23 @@ function App() {
   const todoItems = [
     {
       name: "Buy1 Milk",
-      dueDate: "28/05/2024"
+      dueDate: "28/05/2024",
     },
     {
       name: "Go to College",
-      dueDate: "28/05/2024"
+      dueDate: "28/05/2024",
     },
     {
       name: "Go to College2",
-      dueDate: "28/05/2024"
-    }
+      dueDate: "28/05/2024",
+    },
   ];
 
   return (
     <center className="todo-container">
       <AppName />
-        <AddTodo></AddTodo>
-        <TodoItems todoItems={todoItems}></TodoItems>
+      <AddTodo></AddTodo>
+      <TodoItems todoItems={todoItems}></TodoItems>
     </center>
   );
 }
