97a03631d9544a9366e95ae1a2dcd4378203602e
Mani1008 <mk114003@gmail.com>
2024-06-01 23:23:11 +0530

    5th update

---
+++ Projects/4-calculator-ver-1/src/App.jsx
@@ -0,0 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./App.module.css"
+import Display from "./components/Display";
+
+function App() {
+  return (
+    <>
+      <div className={styles.calculator}>
+        <Display></Display>
+        <div className={styles.buttonsContainer}>
+          <button className={styles.button}>C</button>
+          <button className={styles.button}>1</button>
+          <button className={styles.button}>2</button>
+          <button className={styles.button}>3</button>
+          <button className={styles.button}>+</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
